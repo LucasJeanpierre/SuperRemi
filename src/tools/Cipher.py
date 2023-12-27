c970283b69cc6c5559247c94b963084aec4c4ff5
Lucas Jeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2023-12-27 13:17:56 +0100

    DES Sbox derivations

--- src/tools/Cipher.py
+++ src/tools/Cipher.py
@@ -63,6 +63,48 @@ class SerpentCipher(SymmetricCipher):
         0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80, 84, 88, 92, 96, 100, 104, 108, 112, 116, 120, 124, 1, 5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 65, 69, 73, 77, 81, 85, 89, 93, 97, 101, 105, 109, 113, 117, 121, 125, 2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62, 66, 70, 74, 78, 82, 86, 90, 94, 98, 102, 106, 110, 114, 118, 122, 126, 3, 7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63, 67, 71, 75, 79, 83, 87, 91, 95, 99, 103, 107, 111, 115, 119, 123, 127
     ]
 
+    DES_SBOX = [
+        [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9 ,0, 7],
+        [0, 15, 7, 4, 14, 2, 13 ,1 ,10, 6, 12, 11, 9, 5, 3, 8],
+        [4, 1, 14, 8 ,13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5 ,0],
+        [15, 12, 8 ,2 ,4 ,9 ,1 ,7 ,5 ,11, 3, 14, 10, 0, 6, 13],
+
+        [15, 1 ,8 ,14 ,6 ,11 ,3 ,4 ,9 ,7 ,2 ,13 ,12 ,0 ,5 ,10],
+        [3 ,13 ,4 ,7 ,15 ,2 ,8 ,14 ,12 ,0 ,1 ,10 ,6 ,9 ,11 ,5],
+        [0 ,14 ,7 ,11 ,10 ,4 ,13 ,1 ,5 ,8 ,12 ,6 ,9 ,3 ,2 ,15],
+        [13 ,8 ,10 ,1 ,3 ,15 ,4 ,2 ,11 ,6 ,7 ,12 ,0 ,5 ,14 ,9],
+
+        [10 ,0 ,9 ,14 ,6 ,3 ,15 ,5 ,1 ,13 ,12 ,7 ,11 ,4 ,2 ,8],
+        [13 ,7 ,0 ,9 ,3 ,4 ,6 ,10 ,2 ,8 ,5 ,14 ,12 ,11 ,15 ,1],
+        [13 ,6 ,4 ,9 ,8 ,15 ,3 ,0 ,11 ,1 ,2 ,12 ,5 ,10 ,14 ,7],
+        [1 ,10 ,13 ,0 ,6 ,9 ,8 ,7 ,4 ,15 ,14 ,3 ,11 ,5 ,2 ,12],
+
+        [7 ,13 ,14 ,3 ,0 ,6 ,9 ,10 ,1 ,2 ,8 ,5 ,11 ,12 ,4 ,15],
+        [13 ,8 ,11 ,5 ,6 ,15 ,0 ,3 ,4 ,7 ,2 ,12 ,1 ,10 ,14 ,9],
+        [10 ,6 ,9 ,0 ,12 ,11 ,7 ,13 ,15 ,1 ,3 ,14 ,5 ,2 ,8 ,4],
+        [3 ,15 ,0 ,6 ,10 ,1 ,13 ,8 ,9 ,4 ,5 ,11 ,12 ,7 ,2 ,14],
+
+        [2 ,12 ,4 ,1 ,7 ,10 ,11 ,6 ,8 ,5 ,3 ,15 ,13 ,0 ,14 ,9],
+        [14 ,11 ,2 ,12 ,4 ,7 ,13 ,1 ,5 ,0 ,15 ,10 ,3 ,9 ,8 ,6],
+        [4 ,2 ,1 ,11 ,10 ,13 ,7 ,8 ,15 ,9 ,12 ,5 ,6 ,3 ,0 ,14],
+        [11 ,8 ,12 ,7 ,1 ,14 ,2 ,13 ,6 ,15 ,0 ,9 ,10 ,4 ,5 ,3],
+
+        [12 ,1 ,10 ,15 ,9 ,2 ,6 ,8 ,0 ,13 ,3 ,4 ,14 ,7 ,5 ,11],
+        [10 ,15 ,4 ,2 ,7 ,12 ,9 ,5 ,6 ,1 ,13 ,14 ,0 ,11 ,3 ,8],
+        [9 ,14 ,15 ,5 ,2 ,8 ,12 ,3 ,7 ,0 ,4 ,10 ,1 ,13 ,11 ,6],
+        [4 ,3 ,2 ,12 ,9 ,5 ,15 ,10 ,11 ,14 ,1 ,7 ,6 ,0 ,8 ,13],
+
+        [4 ,11 ,2 ,14 ,15 ,0 ,8 ,13 ,3 ,12 ,9 ,7 ,5 ,10 ,6 ,1],
+        [13 ,0 ,11 ,7 ,4 ,9 ,1 ,10 ,14 ,3 ,5 ,12 ,2 ,15 ,8 ,6],
+        [1 ,4 ,11 ,13 ,12 ,3 ,7 ,14 ,10 ,15 ,6 ,8 ,0 ,5 ,9 ,2],
+        [6 ,11 ,13 ,8 ,1 ,4 ,10 ,7 ,9 ,5 ,0 ,15 ,14 ,2 ,3 ,12],
+
+        [13 ,2 ,8 ,4 ,6 ,15 ,11 ,1 ,10 ,9 ,3 ,14 ,5 ,0 ,12 ,7],
+        [1 ,15 ,13 ,8 ,10 ,3 ,7 ,4 ,12 ,5 ,6 ,11 ,0 ,14 ,9 ,2],
+        [7 ,11 ,4 ,1 ,9 ,12 ,14 ,2 ,0 ,6 ,10 ,13 ,15 ,3 ,5 ,8],
+        [2 ,1 ,14 ,7 ,4 ,10 ,8 ,13 ,15 ,12 ,9 ,0 ,3 ,5 ,6 ,11]
+    ]
+
     def encrypt(self, plaintext):
         """
         Serpent encryption
@@ -83,6 +125,11 @@ class SerpentCipher(SymmetricCipher):
             block = ciphertext[i:i+128]
             ciphertext[i:i+128] = self.blockEncryption(block)
 
+        # Final permutation
+        for i in range(0, len(ciphertext), 128):
+            block = ciphertext[i:i+128]
+            ciphertext[i:i+128] = self.blockFinalPermutation(block)
+
         return ciphertext.to01()
 
 
@@ -96,11 +143,16 @@ class SerpentCipher(SymmetricCipher):
         blocks = self.splitCipherTextToBlocks(ciphertext)
         plaintext = bitarray.bitarray()
 
-        # Block decryption
+        # Reverse Final permutation
         for block in blocks:
-            plaintext.extend(self.blockDecryption(block))
+            plaintext.extend(self.blockInitialPermutation(block))
+
+        # Block decryption
+        for i in range(0, len(plaintext), 128):
+            block = plaintext[i:i+128]
+            plaintext[i:i+128] = self.blockDecryption(block)
         
-        # Final permutation
+        # Reverse Initial permutation
         for i in range(0, len(plaintext), 128):
             block = plaintext[i:i+128]
             plaintext[i:i+128] = self.blockFinalPermutation(block)
@@ -157,8 +209,9 @@ class SerpentCipher(SymmetricCipher):
 
         # Sbox
         for i in range(0, len(block), 4):
+            index = i // 4
             current = block[i:i+4]
-            result = bin(_SBox[int(current.to01(), 2)])
+            result = bin(_SBox[index][int(current.to01(), 2)])
             result = result[2:].zfill(4)
             block[i:i+4] = bitarray.bitarray(result)
         
@@ -182,8 +235,9 @@ class SerpentCipher(SymmetricCipher):
 
         # Sbox
         for i in range(0, len(block), 4):
+            index = i // 4
             current = block[i:i+4]
-            result = bin(_SBox.index(int(current.to01(), 2)))
+            result = bin(_SBox[index].index(int(current.to01(), 2)))
             result = result[2:].zfill(4)
             block[i:i+4] = bitarray.bitarray(result)
 
@@ -315,12 +369,19 @@ class SerpentCipher(SymmetricCipher):
         """
         SBox generation
         """
-        SBox = [[] for _ in range(32)]
-        _table = [i for i in range(16)]
-        for i in range(32):
-            random.shuffle(_table)
-            SBox[i] = _table.copy()
-            
+        SBox = [[[i for i in  range(16)] for _ in range(32)] for _ in range(32)]
+        SBox[0] = SerpentCipher.DES_SBOX
+        for i in range(1,32):
+            SBox[i] = SBox[i-1].copy()
+            current_sbox = SBox[i]
+            for index_box in range(32):
+                for index_bit in range(16):
+                    a = index_bit + current_sbox[index_box][index_bit]
+                    b = current_sbox[a][index_bit]
+                    _temp = current_sbox[index_box][index_bit]
+                    current_sbox[index_box][index_bit] = current_sbox[index_box][b]
+                    current_sbox[index_box][b] = _temp
+        print(SBox[31])
         return SBox
     
 
@@ -388,7 +449,6 @@ class SerpentCipher(SymmetricCipher):
         """
         return bitarray.bitarray([block[i] for i in SerpentCipher.FPTable])
     
-
 
 
 class RSA(AsymmetricCipher):
